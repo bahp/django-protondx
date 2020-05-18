75c20a90026313bb63921618f2d7fe0b7c1aee24
ostiff <37678137+ostiff@users.noreply.github.com>
2020-05-19 09:48:06 +1000

    Update: settings and install guide

--- protondx/protondx/settings.py
+++ protondx/protondx/settings.py
@@ -94,22 +94,13 @@ WSGI_APPLICATION = 'protondx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'sample_database',
-        'USER': 'oliver_django',
-        'PASSWORD': '1234567T',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.contrib.gis.db.backends.spatialite',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
 # Password validation

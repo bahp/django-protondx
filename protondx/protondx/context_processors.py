07846305094c712e20ed34adf55caeebf192eff6
ostiff <37678137+ostiff@users.noreply.github.com>
2020-06-11 17:26:09 +1000

    Add: URL scheme env var

--- protondx/protondx/context_processors.py
+++ protondx/protondx/context_processors.py
@@ -5,5 +5,6 @@ def export_vars(request):
     data = {
         'PROJECT_NAME': os.getenv('APP_NAME'),
         'HOSTNAME': "localhost:8000" if os.getenv('SETTINGS') == "DEV" else os.getenv('WEB_HOST'),
+        'URL_SCHEME': "http" if os.getenv('SETTINGS') == "DEV" else "https",
     }
     return data

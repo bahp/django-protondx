718c461618a95ed49bd68446a186a288494ff3b3
ostiff <37678137+ostiff@users.noreply.github.com>
2020-06-12 16:43:59 +1000

    Update: PCR chart and modal close

--- protondx/dashboard/static/dashboard/js/dropDown.js
+++ protondx/dashboard/static/dashboard/js/dropDown.js
@@ -1,16 +1,10 @@
 function openDropdown() {
     document.getElementById("dropdown").classList.toggle("show");
 }
-const modal = document.getElementById("myModal");
-
-
 // When the user clicks anywhere outside of the modal, close it
 // Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-    else if (!event.target.matches('.dropButton')) {
+window.addEventListener('click', function(event) {
+    if (!event.target.matches('.dropButton')) {
         let dropdowns = document.getElementsByClassName("dropdown-content");
         let i;
         for (i = 0; i < dropdowns.length; i++) {
@@ -20,4 +14,4 @@ window.onclick = function(event) {
             }
         }
     }
-}
+});
